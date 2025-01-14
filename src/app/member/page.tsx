cf07fd86c7b2490488c6905392583caecd2eb440
shio-max <71300915+shio-max@users.noreply.github.com>
2025-01-15 02:24:36 +0900

    メンバ一覧に絞り込み検索機能を追加

--- src/app/member/page.tsx
+++ src/app/member/page.tsx
@@ -14,9 +14,15 @@ const Member = () => {
   const [showMemberModal, setShowMemberModal] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
   const [memberList, setMemberList] = useState<MemberInfo[]>([]);
+  const [filteredMemberList, setFilteredMemberList] = useState<MemberInfo[]>(
+    []
+  );
   const [isLoading, setIsLoading] = useState(false);
   const [selectedMember, setSelectedMember] = useState<MemberInfo | null>(null);
   const [deletedMemberList, setDeletedMemberList] = useState(false);
+  const [searchName, setSearchName] = useState("");
+  const [searchEmail, setSearchEmail] = useState("");
+  const [searchBelongTo, setSearchBelongTo] = useState("");
 
   const handleEdit = (member: MemberInfo) => {
     setSelectedMember(member);
@@ -66,7 +72,7 @@ const Member = () => {
     return baseHeaders;
   };
 
-  const handleFilterList = async () => {
+  const handleDeletedFilterList = async () => {
     const newDeletedMemberList = !deletedMemberList;
     setDeletedMemberList(newDeletedMemberList);
     await getMembers(newDeletedMemberList);
@@ -77,6 +83,7 @@ const Member = () => {
       setIsLoading(true);
       const membersData = await fetchMembers(isDeletedMemberList);
       setMemberList(membersData);
+      setFilteredMemberList(membersData);
     } catch (error) {
       console.error("Error fetching member list:", error);
     } finally {
@@ -84,10 +91,29 @@ const Member = () => {
     }
   };
 
-  const fetch = async () => {
+  const handleReset = async () => {
+    setSearchName("");
+    setSearchEmail("");
+    setSearchBelongTo("");
     setDeletedMemberList(false);
     await getMembers(false);
   };
+
+  useEffect(() => {
+    // 入力フィールドが空の場合、元のリストを表示
+    if (!searchName && !searchEmail && !searchBelongTo) {
+      setFilteredMemberList(memberList);
+    } else {
+      const filteredMembers = memberList.filter((member) => {
+        return (
+          member.name.includes(searchName) &&
+          member.email.includes(searchEmail) &&
+          member.belong_to.includes(searchBelongTo)
+        );
+      });
+      setFilteredMemberList(filteredMembers);
+    }
+  }, [searchName, searchEmail, searchBelongTo, memberList]);
 
   useEffect(() => {
     getMembers(false);
@@ -96,43 +122,73 @@ const Member = () => {
   return (
     <>
       <Loading show={isLoading} />
-      <div className="p-6 bg-gray-100 min-h-screen">
-        <div className="p-6 bg-white shadow-md rounded-lg">
-          <h2 className="text-xl font-bold text-gray-800 mb-2">メンバ一覧</h2>
-          <div className="flex items-center justify-between mb-4">
-            <div className="flex w-full">
+
+      <div className="p-6 bg-white">
+        <h2 className="text-xl font-bold text-gray-800 mb-2">メンバ一覧</h2>
+        <div className="flex items-end justify-between mb-4">
+          {/* 絞り込み検索フィールド */}
+          <div>
+            <div className="flex gap-x-2 items-center">
+              <input
+                type="text"
+                placeholder="名前で検索"
+                className="border p-2 rounded"
+                value={searchName}
+                onChange={(e) => setSearchName(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="メールアドレスで検索"
+                className="border p-2 rounded"
+                value={searchEmail}
+                onChange={(e) => setSearchEmail(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="所属で検索"
+                className="border p-2 rounded"
+                value={searchBelongTo}
+                onChange={(e) => setSearchBelongTo(e.target.value)}
+              />
+              <Button className="bg-gray w-32" onClick={handleReset}>
+                リセット
+              </Button>
+            </div>
+            <div className="flex items-center mt-4 ">
               <Checkbox
                 checked={deletedMemberList}
-                onChange={handleFilterList}
-                className="mx-4 group size-6 rounded-sm bg-gray-100 p-1 ring-1 checked:bg-secondary"
+                onChange={handleDeletedFilterList}
+                className="mx-2 group size-6 rounded-sm bg-gray-100 p-1 ring-1 checked:bg-secondary"
               >
                 {deletedMemberList && (
                   <FaCheck className="text-primary size-4" />
                 )}
               </Checkbox>
-              <span>削除済みメンバを表示する</span>
+              <span className="text-sm">削除済みメンバを表示する</span>
             </div>
-            <Button className="bg-primary" onClick={handleAddClick}>
-              メンバ追加
-            </Button>
           </div>
-          <Table
-            headers={getHeaders()}
-            items={memberList}
-            onIsEditChange={handleEdit}
-            onDeleteChange={handleDelete}
-          />
-        </div>
+          {/* END 絞り込み検索フィールド */}
 
-        {showMemberModal && (
-          <MemberModal
-            isEdit={isEdit}
-            selectedMember={selectedMember}
-            onClose={() => setShowMemberModal(false)}
-            onSaveSuccess={fetch}
-          />
-        )}
+          <Button className="bg-primary" onClick={handleAddClick}>
+            メンバ追加
+          </Button>
+        </div>
+        <Table
+          headers={getHeaders()}
+          items={filteredMemberList}
+          onIsEditChange={handleEdit}
+          onDeleteChange={handleDelete}
+        />
       </div>
+
+      {showMemberModal && (
+        <MemberModal
+          isEdit={isEdit}
+          selectedMember={selectedMember}
+          onClose={() => setShowMemberModal(false)}
+          onSaveSuccess={getMembers.bind(null, false)}
+        />
+      )}
     </>
   );
 };
